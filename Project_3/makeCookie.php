cd3ab9c6c1969e69f8b2166756ce50785bdea139
cskevingVillanova <cskeving@villanova.edu>
2019-12-06 11:14:05 -0500

    Okay here we go, adding starting files finally

---
+++ Project_3/makeCookie.php
@@ -0,0 +1,15 @@
+
+<?php
+if(isset($_POST['race']) && !empty($_POST['race']) && isset($_POST['class']) && !empty($_POST['class'])) {
+    echo "adding cookies";
+    $cookieName = "race";
+    $cookieValue = $_POST['race'];
+    setcookie($cookieName, $cookieValue);
+    $cookieName = "class";
+    $cookieValue = $_POST['class'];
+    setcookie($cookieName, $cookieValue);
+} else {
+    echo "Please make sure you have selected all of the required information";
+}
+
+header("Location: result.php");
