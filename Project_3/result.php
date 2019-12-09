a3081ca6d4533a7da08ca413852180a80b84b99c
cskevingVillanova <cskeving@villanova.edu>
2019-12-09 10:55:45 -0500

    adding the css file to the top of the thing

--- Project_3/result.php
+++ Project_3/result.php
@@ -1,3 +1,12 @@
+
+  <head>
+           <meta charset="utf-8">
+                <title>Dungeons and Dragons</title>
+                <h1>Dungeons and Dragons</h1>
+                <link type="text/css" rel="stylesheet" href="styles/mystyles.css" media="screen">
+       </head>
+       <div id=banner>
+
 <?php 
 if(!isset($_COOKIE['race']) || !isset($_COOKIE['class'])){
     echo '<p>Persistent favShow cookie not found </p>';
@@ -7,3 +16,4 @@ else {
 }
 
 ?>
+</div>
