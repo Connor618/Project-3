40f3c91d34e5b327d21e5a7f647b00543afc4e8f
cskevingVillanova <cskeving@villanova.edu>
2019-12-09 11:03:32 -0500

    updating files once more

--- Project_3/result.php
+++ Project_3/result.php
@@ -1,11 +1,11 @@
-
+<div id=banner>
   <head>
            <meta charset="utf-8">
                 <title>Dungeons and Dragons</title>
                 <h1>Dungeons and Dragons</h1>
                 <link type="text/css" rel="stylesheet" href="styles/mystyles.css" media="screen">
        </head>
-       <div id=banner>
+       
 
 <?php 
 if(!isset($_COOKIE['race']) || !isset($_COOKIE['class'])){
